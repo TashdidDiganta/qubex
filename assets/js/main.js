38ba99ef3c7c25f320af34513c39f1de38deddff
TashdidDiganta <tashdiddiganta@gmail.com>
2024-09-23 13:55:45 +0600

    breadcrumb working

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+
+(function ($) {
+    "use strict";
+
+
+
+
+
+    
+    
+    // data bg img 
+    $("[data-background]").each(function () {
+        $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
+    })
+
+    // data bg color
+    $("[data-bg-color]").each(function () {
+        $(this).css("background-color", $(this).attr("data-bg-color"))
+    })
+
+    // data bg color
+    $("[data-color]").each(function () {
+        $(this).css("color", $(this).attr("data-color"))
+    })
+
+    $('.popup-image').magnificPopup({
+        type: 'image'
+        // other options
+    });
+    $('.popup-video').magnificPopup({
+        type: 'iframe'
+        // other options
+    });
+
+})(jQuery);
