fa5f12f0b7b1f927e9451e289925bb9c7b2d28e5
TashdidDiganta <tashdiddiganta@gmail.com>
2024-09-25 22:26:34 +0600

    faq left done

--- assets/js/jsPractis.js
+++ assets/js/jsPractis.js
@@ -1,11 +1,26 @@
 
 
 
-function inchToFit(inch){
-    const fite = inch/12
-    return fite;
+function oddAvarage(numbers){
+
+    const odds = [];
+    for(number of numbers){
+        if(number % 2 === 1){
+            odds.push(number);
+        }
+    }
+
+    let sum = 0;
+    for(const oddNumber of odds){
+        sum = sum + oddNumber;
+    }
+    let lenght = odds.length
+    let avg = Math.round(sum/lenght, 4) 
+
+    console.log(avg)
 }
 
-const hight = inchToFit(24);
+const array = [3,52,37,42,21,83];
 
-console.log(hight);
+ oddAvarage(array);
+
